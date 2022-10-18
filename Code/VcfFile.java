0e2a5a0a0dfb2699d31d7a38eb60f8e1b8c63972
Rudel Fankep <rfankep@yahoo.com>
2022-10-18 11:05:05 +0200

    Create VcfFile.java
    This Class is to save the content of vcf files after every change

---
+++ Code/VcfFile.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class VcfFile {
+    String referenceGenome;
+    String memberId;
+    String bogenNr;
+    String erfmit;
+    String datum;
+    ArrayList<String> metadata;
+    ArrayList<String> variants;
+    ArrayList<String> liftedOver;
+    ArrayList<String> normalised;
+
+    public VcfFile(String[] infos){
+        this.referenceGenome = infos[0];
+        this.memberId = infos[1];
+        this.bogenNr = infos[2];
+        this.erfmit = infos[3];
+        this.datum = infos[4];
+
+    }
+
+}
