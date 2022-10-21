fdb32e875fc84d97523a97cf289a6e569a96f4bc
Rudel Fankep <rfankep@yahoo.com>
2022-10-21 11:08:25 +0200

    Update main file with snpeff run

--- Code/ParseVcf.java
+++ Code/ParseVcf.java
@@ -24,6 +24,10 @@ public class ParseVcf {
     }
     
     public static void main(String[] args){
+        String JAVA = "java";
+        String SNPEFF = "C:\\Users\\Rudel\sFankep\\Documents\\GitHub\\HerediCaRe_VCF_Upload\\snpEff\\snpEff.jar";
+        int RAMVALUE = 3;
+        String GENOME = "GRCh38.mane.1.0.refseq";
 
         // ToDo parse arguments later
 
@@ -44,47 +48,74 @@ public class ParseVcf {
         for (String file : vcfToParse) {
             if (file.endsWith(".vcf")){
                 // parse the filename to get some metadata infos = [reference, patientenID, MGU-BogenNr, MitarbeiterID, Zeitstempel]
-                String errorOutput="ok";
-                int i = file.lastIndexOf(".");
-                String[] sfile =  {file.substring(0, i), file.substring(i)};
-                String[] infos=new String[5];
-                if(sfile.length>0){
-                    String[] temp =sfile[0].split("\\-");
-                    if(temp.length == 5 ) infos = temp;
-                    else{ errorOutput= "Fehlerhafter Dateiname";}
-                }else{ errorOutput= "Fehlerhafter Dateiname";}
+                try {
+                    String errorOutput="ok";
+                    int i = file.lastIndexOf(".");
+                    String[] sfile =  {file.substring(0, i), file.substring(i)};
+                    String[] infos=new String[5];
+                    if(sfile.length>0){
+                        String[] temp =sfile[0].split("\\-");
+                        if(temp.length == 5 ) infos = temp;
+                        else{ errorOutput= "Fehlerhafter Dateiname";}
+                    }else{ errorOutput= "Fehlerhafter Dateiname";}
 
-                // create a file object
-                VcfFile fileObject = new VcfFile(infos);
+                    // create a file object
+                    VcfFile fileObject = new VcfFile(infos);
 
-                // save the header and the Variants from vcf file
-                myUtils.saveVcfContent(fileObject, vcfInputDir + "\\" + file); 
-                
-                // normalise the file
-                //String[] returnval_errortext = Normalize.split_lines(VCF_IN_DIR+"\\"+file,getrennt);
-                String[] returnval_errortext = Normalize.split_lines(fileObject.variants);
-                if(!returnval_errortext[0].equals("0")){
-                    System.out.println("ERROR while unraveling");
-                    //write_output_and_move(file,new ArrayList<Transcript>(),new StreamCatcher(returnval_errortext[1]));
-                    continue;
-                }// first split_lines, then normalize und save in a file
+                    // save the header and the Variants from vcf file
+                    myUtils.saveVcfContent(fileObject, vcfInputDir + "\\" + file); 
+                    
+                    // normalise the file
+                    //String[] returnval_errortext = Normalize.split_lines(VCF_IN_DIR+"\\"+file,getrennt);
+                    String[] returnval_errortext = Normalize.split_lines(fileObject.variants);
+                    if(!returnval_errortext[0].equals("0")){
+                        System.out.println("ERROR while unraveling");
+                        //write_output_and_move(file,new ArrayList<Transcript>(),new StreamCatcher(returnval_errortext[1]));
+                        continue;
+                    }// first split_lines, then normalize und save in a file
 
-                System.out.println("Normalize into "+myParser.normalizedFolder);
-                fileObject.normalized = new ArrayList<>(fileObject.metadata);
-                fileObject.normalized.addAll(fileObject.variants);
-                if(Normalize.genenotation(fileObject.normalized,myParser.normalizedFolder+"\\"+file,fileObject.referenceGenome) == 1){
-                    System.out.println("ERROR while normalizing");
-                    //write_output_and_move(file,new ArrayList<Transcript>(),new StreamCatcher("ERROR while normalizing"));
-                    continue;
+                    System.out.println("Normalize into "+myParser.normalizedFolder);
+                    fileObject.normalized = new ArrayList<>(fileObject.metadata);
+                    fileObject.normalized.addAll(fileObject.variants);
+                    if(Normalize.genenotation(fileObject.normalized,myParser.normalizedFolder+"\\"+file,fileObject.referenceGenome) == 1){
+                        System.out.println("ERROR while normalizing");
+                        //write_output_and_move(file,new ArrayList<Transcript>(),new StreamCatcher("ERROR while normalizing"));
+                        continue;
+                    }
+                        //create a new process
+                        System.out.println("Process is running...");
+                        System.out.println(JAVA+" -Xmx"+RAMVALUE+"g -jar \""+SNPEFF+"\" "+ GENOME + " -noLog "+myParser.normalizedFolder+"\\"+file);
+                        Process p = Runtime.getRuntime().exec(JAVA+" -Xmx"+RAMVALUE+"g -jar \""+SNPEFF+"\" "+ GENOME + " -noLog "+myParser.normalizedFolder+"\\"+file);
+                        
+                        
+                        //List<Transcript>  trueTranscripts = new ArrayList<Transcript>();
+                        
+                        //catch the Error Stream
+                        StreamCatcher errC = new StreamCatcher(p.getErrorStream());
+                        errC.start();
+                        //Interpreting the Output Stream 
+                        BufferedReader pout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        
+                        // for every line in snpEff output
+                        for(String s=pout.readLine(); s!=null; s=pout.readLine()){
+                            System.out.println("one"+s);
+
+                        }
+                        
+                        //Rückgabewert des SNPEFF Programmes
+                        p.waitFor();
+                    
+
+                    // save the lifted over part
+                    //fileObject.liftedOver = hgLiftOver.encoding_and_liftover(fileObject.variants, fileObject.referenceGenome);
+                    //System.out.println("origin"+ fileObject.variants);
+                    //System.out.println("lifted"+ fileObject.liftedOver);
+                    //hgLiftOver.l
+                }catch (Exception ex) {
+                    System.out.println("Process handeling "+file+" failed.");
+                    ex.printStackTrace();
                 }
-                System.out.println(fileObject.normalized);
                 
-
-                // save the lifted over part
-                //fileObject.liftedOver = hgLiftOver.encoding_and_liftover(fileObject.variants, fileObject.referenceGenome);
-                //System.out.println("origin"+ fileObject.variants);
-                //System.out.println("lifted"+ fileObject.liftedOver);
-                //hgLiftOver.l
             }
         //System.out.println(transcriptMap);
             
